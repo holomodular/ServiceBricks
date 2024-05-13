9a9df324351f9bbcf8f7088c9229162ed30b8b74
Danny Logsdon <danny.logsdon@holomodular.com>
2024-05-13 07:48:22 -0400

    Fix for xunit tests

---
+++ src/V1/Tests/ServiceBricks.XunitNet7/DummyTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBricks.XunitNet7
+{
+    public class DummyTest
+    {
+        [Fact]
+        public void Test()
+        {
+        }
+    }
+}
