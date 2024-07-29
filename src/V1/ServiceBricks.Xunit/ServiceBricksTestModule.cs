c88d209266a66c7d031b58f3f8bf871086edcf79
Danny Logsdon <danny.logsdon@holomodular.com>
2024-07-29 11:55:54 -0400

    Add missing file

---
+++ src/V1/ServiceBricks.Xunit/ServiceBricksTestModule.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace ServiceBricks.Xunit
+{
+    public class ServiceBricksTestModule : IModule
+    {
+        public ServiceBricksTestModule()
+        {
+        }
+
+        public List<Assembly> AutomapperAssemblies { get; }
+        public List<Assembly> ViewAssemblies { get; }
+        public List<IModule> DependentModules { get; }
+    }
+}
