74cf919aec6bfdae3e9c4d9bb27f20d8d65b0e4c
Danny Logsdon <danny.logsdon@holomodular.com>
2024-11-30 18:42:58 -0500

    v1.1.2 * Create rule for MongoDb GuidSerializer default rule

---
+++ src/V1/ServiceBricks.Storage.MongoDb/Rule/MongoDbGuidSerializerStandardRule.cs
@@ -0,0 +1,67 @@
+﻿using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson;
+
+namespace ServiceBricks.Storage.MongoDb
+{
+    /// <summary>
+    /// This rule migrates a database.
+    /// </summary>
+    public sealed class MongoDbGuidSerializerStandardRule : BusinessRule
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MongoDbGuidSerializerStandardRule()
+        {
+            Priority = PRIORITY_LOW;
+        }
+
+        /// <summary>
+        /// Register the rule
+        /// </summary>
+        public static void Register(IBusinessRuleRegistry registry)
+        {
+            registry.Register(
+                typeof(ModuleStartEvent<ServiceBricksModule>),
+                typeof(MongoDbGuidSerializerStandardRule));
+        }
+
+        /// <summary>
+        /// UnRegister the rule.
+        /// </summary>
+        public static void UnRegister(IBusinessRuleRegistry registry)
+        {
+            registry.UnRegister(
+                typeof(ModuleStartEvent<ServiceBricksModule>),
+                typeof(MongoDbGuidSerializerStandardRule));
+        }
+
+        /// <summary>
+        /// Execute the business rule.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override IResponse ExecuteRule(IBusinessRuleContext context)
+        {
+            var response = new Response();
+
+            // AI: Make sure the context object is the correct type
+            if (context == null || context.Object == null)
+            {
+                response.AddMessage(ResponseMessage.CreateError(LocalizationResource.PARAMETER_MISSING, "context"));
+                return response;
+            }
+            var e = context.Object as ModuleStartEvent<ServiceBricksModule>;
+            if (e == null || e.DomainObject == null || e.ApplicationBuilder == null)
+            {
+                response.AddMessage(ResponseMessage.CreateError(LocalizationResource.PARAMETER_MISSING, "context"));
+                return response;
+            }
+
+            BsonSerializer.RegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
+
+            return response;
+        }
+    }
+}
